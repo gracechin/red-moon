cf04ef3ddc29492fa6b32f34d0ec05df84e2b284
Grace <27grace@gmail.com>
2023-06-12 18:49:41 +0800

    Generate in-between missing daily entries

--- src/utils/dataStorage.js
+++ src/utils/dataStorage.js
@@ -1,5 +1,10 @@
-import { dateComparison } from "../utils/dateTime";
-
+import {
+  dateComparison,
+  datesDiffInDays,
+  transformDateToDateStr,
+  newDateByDiff,
+} from "../utils/dateTime";
+import { range } from "../utils/utils";
 const DATA_ENTRIES_KEY = "Entries";
 
 export const getStored = (key) => JSON.parse(localStorage.getItem(key));
@@ -8,6 +13,24 @@ export const store = (key, value) =>
   localStorage.setItem(key, JSON.stringify(value));
 
 export const getStoredEntries = () => getStored(DATA_ENTRIES_KEY) || [];
+
+const getMissingEntries = (entries, newEntry) => {
+  if (entries.length == 0) return [];
+  const newEntryDate = new Date(newEntry.Date);
+  const dates = entries.map((e) => new Date(e.Date));
+  const maxDate = new Date(Math.max.apply(null, dates));
+  const minDate = new Date(Math.min.apply(null, dates));
+  const diffWithMaxDate = datesDiffInDays(maxDate, newEntryDate);
+  const diffWithMinDate = datesDiffInDays(minDate, newEntryDate);
+  if (diffWithMaxDate == 1 || diffWithMinDate == 1) return [];
+  const missingDates =
+    diffWithMaxDate > diffWithMinDate
+      ? range(1, diffWithMinDate).map((i) => newDateByDiff(minDate, -i))
+      : range(1, diffWithMaxDate).map((i) => newDateByDiff(maxDate, i));
+  return missingDates.map((d) => ({
+    Date: transformDateToDateStr(d),
+  }));
+};
 
 export const storeEntries = (entries) => {
   const sortedEntries = entries.sort((e1, e2) =>
@@ -18,7 +41,11 @@ export const storeEntries = (entries) => {
 
 export const storeEntry = (entry) => {
   var entries = getStoredEntries() || [];
-  entries = entries.filter((e) => e.Date != entry.Date);
+  if (entries.map((e) => e.Date).includes(entry.Date)) {
+    entries = entries.filter((e) => e.Date != entry.Date);
+  } else {
+    entries = [...entries, ...getMissingEntries(entries, entry)];
+  }
   entries.push(entry);
   storeEntries(entries);
 };
--- src/utils/dateTime.js
+++ src/utils/dateTime.js
@@ -1,10 +1,14 @@
-export const getCurrentDate = () => {
-  const date = new Date().toLocaleString().split(" ");
+export const transformDateToDateStr = (d) => {
+  const date = d.toLocaleString().split(" ");
   const [day, month, year] = date[0].split("/").map((s) => s.replace(",", ""));
   return [year, month, day].join("-");
 };
 
-export const getCurrentTime = () => {
+export const getCurrentDateStr = () => {
+  return transformDateToDateStr(new Date());
+};
+
+export const getCurrentTimeStr = () => {
   const date = new Date().toLocaleString().split(" ");
   const [hh, mm, _ss] = date[1].split(":");
   return [hh, mm].join(":");
@@ -14,4 +18,15 @@ export const dateComparison = (a, b) => {
   const date1 = new Date(a);
   const date2 = new Date(b);
   return date1 - date2;
+};
+
+export const datesDiffInDays = (d1, d2) => {
+  const timeDiff = Math.abs(d1.getTime() - d2.getTime());
+  return timeDiff / (1000 * 3600 * 24);
+};
+
+export const newDateByDiff = (d, diff) => {
+  const nd = new Date();
+  nd.setDate(d.getDate() + diff);
+  return nd;
 };
